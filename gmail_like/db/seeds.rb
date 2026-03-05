e688b004ac4b3479330e6b6bcc7cba8a3d0ec888
crousty24-bit <allen.kch24@gmail.com>
2026-03-05 11:38:49 +0100

    add scaffold email with object & body attributes
    add root index & seed with emails examples
    
    add gem faker & colorize to improve seed generation & better readability

--- gmail_like/db/seeds.rb
+++ gmail_like/db/seeds.rb
@@ -1,9 +1,20 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+require 'faker'
+require 'colorize'
+
+puts "Nettoyage de la base de données...".colorize(:light_blue)
+Email.destroy_all
+
+puts "Création de 5 emails de test...".colorize(:light_blue)
+
+5.times do
+  Email.create!(
+    object: Faker::Lorem.sentence(word_count: 4),
+    body: Faker::Lorem.paragraph(sentence_count: 5)
+  )
+end
+
+puts "Les emails ont été créés avec succès !".colorize(:green)
